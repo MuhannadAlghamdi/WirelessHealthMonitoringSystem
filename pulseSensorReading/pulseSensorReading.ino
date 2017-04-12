901ac479dfa074b59f6f8777b974aab1ba046327
MUHANNAD <alghamdi1992@gmail.com>
2017-04-12 01:58:05 -0700

    UPDATE

--- pulseSensorReading/pulseSensorReading.ino
+++ pulseSensorReading/pulseSensorReading.ino
@@ -1,10 +1,10 @@
 int pulseSensorPin0 = 0;    // The analog pin is connected to pulse sensor.
-int ledPin3 = 3;            // LED light is connected to pin 3.
+int ledPin13 = 13;          // LED light is connected to pin 13.
 int threshold = 550;        // The best beat signal.
 int signalReading;          // Store pulse sensor value.
 
 void setup() {
-    pinMode(ledPin3, OUTPUT);
+    pinMode(ledPin13, OUTPUT);
     // Set's up Serial Communication at certain speed.
     Serial.begin(9600); 
 }
@@ -17,11 +17,11 @@ void loop() {
 
     // If the signal is above "550", then "turn-on" Arduino's on-Board LED.  
     if (signalReading > threshold) {
-        digitalWrite(ledPin3,HIGH);          
+        digitalWrite(ledPin13,HIGH);          
     }
     // The sigal must be below "550", so "turn-off" this LED.
     else {
-        digitalWrite(ledPin3,LOW);
+        digitalWrite(ledPin13,LOW);
     }
 
     delay(10);
